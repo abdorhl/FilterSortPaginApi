88a08606c584f567ec360cb7f86a35c8ddad5ef3
abdorhl <abdltif.rhl28@gmail.com>
2024-10-17 12:20:47 +0100

    Add project files.

---
+++ FilterSortPagin/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using FilterSortPagin.DbContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sieve.Models;
+using Sieve.Services;
+using FilterSortPagin.DbContext;
+
+namespace FilterSortPagin.Controllers
+{
+    
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDB _productDB;
+        private readonly SieveProcessor _sieveProcessor;
+
+        public ProductController(ProductDB productDB, SieveProcessor sieveProcessor)
+        {
+            _productDB = productDB;
+            _sieveProcessor = sieveProcessor;
+        }
+
+        [Route("GetProducts")]
+        [HttpGet]
+        public async Task<IActionResult> GetProducts([FromQuery] SieveModel model)
+        {
+            var products = _productDB.ProductDataContext.AsQueryable();
+            products = _sieveProcessor.Apply(model, products);
+            return Ok();
+        }
+
+    }
+}
---
+++ FilterSortPagin/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿using Sieve.Attributes;
+
+namespace FilterSortPagin.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        [Sieve(CanFilter = true, CanSort = true)] public string Name { get; set; }
+        public string Description { get; set; }
+        [Sieve(CanFilter = true, CanSort = true)] public decimal Price { get; set; }
+    }
+}
